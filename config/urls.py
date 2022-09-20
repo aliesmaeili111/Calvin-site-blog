35de20c7457d97c5a681ec10a2cbcba8dbb5903e
ali esmaeili <aliesmaeili1177@gmail.com>
2022-09-20 16:30:33 +0430

    Edit yrls.py and .env file

--- config/urls.py
+++ config/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     
     path("register/", Register.as_view(), name="register"),
     
-    re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         activate, name='activate'),
  
     path('', include('blog.urls')),
